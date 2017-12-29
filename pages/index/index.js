ea2204fc19c488590f8d7d7e843cb5b06da04e79
qvefofo <363908438@qq.com>
2017-12-29 18:02:28 +0800

    29号

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,6 +15,23 @@ Page({
       url: '../logs/logs'
     })
   },
+  //下拉刷新
+  onPullDownRefresh: function(){
+    wx.request({
+      url: '',
+      data: {},
+      method: 'GET',
+      success: function (res) {},
+      fail: function (res) {},
+      complete: function (res) {
+        wx.stopPullDownRefresh();
+      }
+    })
+  },
+  //分享按钮
+  onShareAppMessage: function (options){
+    console.log(1);
+  },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
@@ -43,8 +60,57 @@ Page({
       })
     }
   },
+  bindAction: function () {
+    console.log('进入登录事件');
+    wx.login({
+      success: (res) => {
+        console.log('首次登录',res);
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code',
+            data: {
+              code: res.code
+            }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+        wx.hideLoading();
+        wx.getUserInfo({
+          withCredentials: false,
+          success: (res) => {
+            console.log(res);
+            this.setData({
+              userInfo: {
+                avatarUrl: res.userInfo.avatarUrl,
+                nickName: res.userInfo.nickName
+              },
+              bType: "warn",
+              actionText: "退出登录"
+            });
+            // 存储用户信息到本地
+            wx.setStorage({
+              key: 'userInfo',
+              data: {
+                userInfo: {
+                  avatarUrl: res.userInfo.avatarUrl,
+                  nickName: res.userInfo.nickName
+                },
+                bType: "warn",
+                actionText: "退出登录"
+              },
+              success: function (res) {
+                console.log("存储成功")
+              }
+            })
+          }
+        })
+      }
+    })
+  },
   getUserInfo: function(e) {
-    console.log(e)
+    // console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
